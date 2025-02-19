44adff2e4a7a89456288f85ae9a8c0cd0df3ec0e
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-02-19 10:28:13 -0600

    add loading fallback display

--- 15-http-requests/src/components/AvailablePlaces.jsx
+++ 15-http-requests/src/components/AvailablePlaces.jsx
@@ -3,14 +3,18 @@ import {useEffect, useState} from "react";
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isLoading, setIsLoading] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function loadPlaces() {
+      setIsLoading(true);
+
       const res = await fetch('http://localhost:3000/places');
       const data = await res.json();
 
       setAvailablePlaces(data);
+      setIsLoading(false);
     }
 
     loadPlaces();
@@ -20,6 +24,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isLoading}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
