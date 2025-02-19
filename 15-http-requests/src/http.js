5c6abe143af2a349b3ceba6fdbc0532237e0b607
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-02-19 11:14:25 -0600

    save user data to backend

--- 15-http-requests/src/http.js
+++ 15-http-requests/src/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces() {
 
   return data.places;
 }
+
+export async function updateUserPlaces(places) {
+  const res = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({ places }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error('Failed to update user data.');
+  }
+
+  return data.message;
+}
