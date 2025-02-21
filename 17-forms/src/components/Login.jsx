0b9aa6ad6a41a5ba9d58aaa7ba72b5501f4296e0
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-02-21 15:59:29 -0600

    handle form data with state

--- 17-forms/src/components/Login.jsx
+++ 17-forms/src/components/Login.jsx
@@ -1,8 +1,22 @@
+import {useState} from "react";
+
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log('submission prevented');
+    console.log(`User info - Email: ${formData.email} Password: ${formData.password}`);
+  }
+
+  function handleInputChange(key, value) {
+    setFormData(prevData => ({
+      ...prevData,
+      [key]: value,
+    }));
   }
 
   return (
@@ -12,12 +26,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={(e) => handleInputChange('email', e.target.value)}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={(e) => handleInputChange('password', e.target.value)}
+          />
         </div>
       </div>
 
