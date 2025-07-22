086f6460f477a9d40db54946251d8b77bf404f0c
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-07-21 19:49:52 -0500

    add components to display meal information

---
+++ 19-food-order-app/src/components/MealInfo.jsx
@@ -0,0 +1,17 @@
+export default function MealInfo({ name, description, price, image }) {
+  return (
+    <li className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${image}`} alt={name} />
+        <div>
+          <h3>{ name }</h3>
+          <p className="meal-item-price">{ price }</p>
+          <p className="meal-item-description">{ description }</p>
+        </div>
+        <p className="meal-item-actions">
+          <button>Add to Cart</button>
+        </p>
+      </article>
+    </li>
+  );
+}
---
+++ 19-food-order-app/src/components/Meals.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import MealInfo from "./MealInfo.jsx";
+
+export default function Meals() {
+  const [ meals, setMeals ] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const res = await fetch('http://localhost:3000/meals');
+
+      if (!res.ok) {
+        return;
+      }
+
+      const mealData = await res.json();
+
+      setMeals(mealData);
+    }
+
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {meals.map(meal => (
+        <MealInfo
+          key={meal.id}
+          name = {meal.name}
+          description = {meal.description}
+          price = {meal.price}
+          image={meal.image} />
+      ))}
+    </ul>
+  );
+}
