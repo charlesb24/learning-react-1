af653518a985b5980098bf7607ca59c101cbb1c5
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-07-21 19:28:49 -0500

    add project starter files and preliminary header component

---
+++ 19-food-order-app/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import logo from '../assets/logo.jpg';
+
+export default function Header() {
+  return (
+    <header id="main-header">
+
+      <div id="title">
+        <img src={logo} alt="Restaurant logo" />
+        <h1>ReactFood</h1>
+      </div>
+
+      <nav>
+        <button>Cart [0]</button>
+      </nav>
+
+    </header>
+  );
+}
