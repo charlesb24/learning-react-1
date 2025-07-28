93a2abd2fb98ff39bbbe0324d866e138f9015c11
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-07-28 18:49:22 -0500

    create, provide, and consume redux store

---
+++ 20-redux/src/components/Header.js
@@ -0,0 +1,24 @@
+import classes from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href='/20-redux/public'>My Products</a>
+          </li>
+          <li>
+            <a href='/20-redux/public'>My Sales</a>
+          </li>
+          <li>
+            <button>Logout</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
