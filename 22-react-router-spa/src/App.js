22429628ffa103b1282a7991a8cc1f7526da29f0
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-08-05 15:45:23 -0500

    use react-router-dom to navigate between pages and wrap all routes in a root element

--- 22-react-router-spa/src/App.js
+++ 22-react-router-spa/src/App.js
@@ -1,10 +1,18 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Root from './pages/Root';
 import Home from './pages/Home';
 import Products from './pages/Products';
 
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/products', element: <Products /> },
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/products', element: <Products /> },
+    ]
+  },
 ]);
 
 function App() {
--- 22-react-router-spa/src/pages/Home.js
+++ 22-react-router-spa/src/pages/Home.js
@@ -1,6 +1,11 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
   return (
-    <h1>My Homepage</h1>
+    <>
+      <h1>My Homepage</h1>
+      <p>View the list of <Link to="/products">products</Link>.</p>
+    </>
   );
 }
 
