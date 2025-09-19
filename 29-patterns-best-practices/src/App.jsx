53ff533f42cfb774caf0159d72056daf2b13a618
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-09-19 18:25:10 -0500

    use render props to format props based on the function passed as the children prop, use function as prop to generate dynamic keys for list elements, and add debouncing to reduce re-rendering of component as search term changes

--- 29-patterns-best-practices/src/App.jsx
+++ 29-patterns-best-practices/src/App.jsx
@@ -1,4 +1,45 @@
+import savannaImg from './assets/african-savanna.jpg';
+import amazonImg from './assets/amazon-river.jpg';
+import caribbeanImg from './assets/caribbean-beach.jpg';
+import desertImg from './assets/desert-dunes.jpg';
+import forestImg from './assets/forest-waterfall.jpg';
+
 import Accordion from './components/Accordion/Accordion.jsx';
+import SearchableList from './components/SearchableList/SearchableList.jsx';
+import Place from './components/Place.jsx';
+
+const PLACES = [
+  {
+    id: 'african-savanna',
+    image: savannaImg,
+    title: 'African Savanna',
+    description: 'Experience the beauty of nature.',
+  },
+  {
+    id: 'amazon-river',
+    image: amazonImg,
+    title: 'Amazon River',
+    description: 'Get to know the largest river in the world.',
+  },
+  {
+    id: 'caribbean-beach',
+    image: caribbeanImg,
+    title: 'Caribbean Beach',
+    description: 'Enjoy the sun and the beach.',
+  },
+  {
+    id: 'desert-dunes',
+    image: desertImg,
+    title: 'Desert Dunes',
+    description: 'Discover the desert life.',
+  },
+  {
+    id: 'forest-waterfall',
+    image: forestImg,
+    title: 'Forest Waterfall',
+    description: 'Listen to the sound of the water.',
+  },
+];
 
 function App() {
   return (
@@ -30,6 +71,15 @@ function App() {
             </Accordion.Content>
           </Accordion.Item>
         </Accordion>
+      </section>
+
+      <section>
+        <SearchableList items={PLACES} itemKeyFn={item => item.id}>
+          { item => <Place item={item} /> }
+        </SearchableList>
+        <SearchableList items={['a', 'b', 'c']} itemKeyFn={item => item}>
+          { item => item }
+        </SearchableList>
       </section>
     </main>
   );
